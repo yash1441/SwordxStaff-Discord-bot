aa20a572b969f9843d42b62a163cb59e7c08ab7a
Simon <yash1441@yahoo.com>
2025-07-14 17:30:43 +0530

    Adds bot presence and event handling
    Adds new events for message reactions and thread creation.
    Sets the bot's presence to display the game being played.

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,19 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setPresence({
+			activities: [
+				{
+					name: "杖劍傳說：坎斯汀之約",
+					type: ActivityType.Playing,
+					state: "杖劍傳說：坎斯汀之約",
+				},
+			],
+			status: "online",
+		});
 	},
 };
